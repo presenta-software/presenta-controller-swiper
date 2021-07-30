9e68926f12975131c1cd2e75223c655c58c55fe4
abusedmedia <hello@fabiofranchino.com>
2021-07-30 12:21:49 +0200

    added scroll detector form vertical navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import Xwiper from 'xwiper'
+import { Lethargy } from 'lethargy'
 
 const controller = function (routerElement, router, ctrlConfig, projectConfig) {
   const xwiperEl = new Xwiper(routerElement.parentNode)
-  xwiperEl.onSwipeLeft(router.next)
-  xwiperEl.onSwipeRight(router.prev)
+  const isVertical = ctrlConfig.vertical
+
+  if (isVertical) {
+    xwiperEl.onSwipeDown(router.next)
+    xwiperEl.onSwipeUp(router.prev)
+
+    const lethargy = new Lethargy()
+    let dontProceed = false
+
+    const callb = e => {
+      // e.preventDefault()
+      // e.stopPropagation()
+
+      const v = lethargy.check(e)
+
+      if (v !== false) {
+        if (dontProceed) return false
+
+        if (v === -1) router.next()
+        if (v === 1) router.prev()
+
+        dontProceed = true
+        setTimeout(() => {
+          dontProceed = false
+        }, 300)
+      }
+    }
+
+    ['mousewheel', 'DOMMouseScroll', 'wheel', 'MozMousePixelScroll'].forEach(evt => {
+      window.addEventListener(evt, callb, false)
+    })
+  } else {
+    xwiperEl.onSwipeLeft(router.next)
+    xwiperEl.onSwipeRight(router.prev)
+  }
 }
 
 controller.install = Presenta => {
